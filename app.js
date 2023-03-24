b9e2d26f289be0bee2fcb92f7d3a03bd90c03cf0
Keiji-Mizumura <keijimizumura@icloud.com>
2023-03-24 21:44:09 +0900

    Adding middlewares

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const express = require('express');
 
 const app = express();
 
+// MIDDLEWARE
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Allows the request to continue to the next middleware in line
+});
+
+// MIDDLEWARE
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
