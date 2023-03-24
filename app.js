08ee365c62efc281a4bf197694ba2f30ea510d6f
Keiji-Mizumura <keijimizumura@icloud.com>
2023-03-24 21:52:10 +0900

    Adding different paths to middlewares

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const express = require('express');
 
 const app = express();
 
-// MIDDLEWARE
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+app.use('/', (req, res, next) => {
+    console.log('Thsi always run!');
+    next();
+});
+
+app.use('/add-product', (req, res, next) =>{
+    console.log("Add product middleware");
+    res.send('<h1>Thhe add product page</h1>');
 });
 
 // MIDDLEWARE
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send('<h1>Hello from Express</h1>');
 });
